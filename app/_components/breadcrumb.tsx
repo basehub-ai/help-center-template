7dbb7e05d6e38bd51864c6acae0a2dfc705b4e37
Joaquin Montes <joaco.rm512@gmail.com>
2024-05-23 14:04:33 -0300

    chore: truncate breadcrumb items

--- app/_components/breadcrumb.tsx
+++ app/_components/breadcrumb.tsx
@@ -1,6 +1,7 @@
-import { Flex, Link, Text } from '@radix-ui/themes'
+import { Box, Flex, Link, Text } from '@radix-ui/themes'
 import { SlashIcon } from '@radix-ui/react-icons'
 import NextLink from 'next/link'
+
 export const Breadcrumb = ({
   category,
   article,
@@ -11,7 +12,12 @@ export const Breadcrumb = ({
   return (
     <Flex gap="1" mb={{ initial: '4', md: '5' }} align="center">
       <Link asChild color="gray" size={{ initial: '2', md: '3' }}>
-        <NextLink href="/">Index</NextLink>
+        <NextLink href="/">
+          <Box asChild display={{ initial: 'none', md: 'inline' }}>
+            <span>All </span>
+          </Box>
+          Categories
+        </NextLink>
       </Link>
 
       <Text color="gray" asChild>
@@ -22,6 +28,7 @@ export const Breadcrumb = ({
         color="gray"
         highContrast={!article}
         size={{ initial: '2', md: '3' }}
+        truncate
       >
         <NextLink href={`/${category._slug}`}>{category._title}</NextLink>
       </Link>
@@ -36,6 +43,7 @@ export const Breadcrumb = ({
             color="gray"
             highContrast
             size={{ initial: '2', md: '3' }}
+            truncate
           >
             <NextLink href={`/${category._slug}/${article._slug}`}>
               {article._title}
