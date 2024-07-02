93d3c60a25bd58ac67b5731ab826b2690a23c524
Joaquin Montes <joaco.rm512@gmail.com>
2024-07-02 10:19:12 -0300

    consume user from cookie

--- app/_components/intercom.tsx
+++ app/_components/intercom.tsx
@@ -1,5 +1,5 @@
 'use client'
-
+import Cookie from 'js-cookie'
 import * as React from 'react'
 import { IntercomProvider as Provider, useIntercom } from 'react-use-intercom'
 
@@ -22,12 +22,33 @@ function IntercomBooter() {
   const { boot, show } = useIntercom()
 
   React.useEffect(() => {
-    boot()
+    const userFromCookie = Cookie.get('intercom-user')
+    if (userFromCookie) {
+      let user
+      try {
+        user = JSON.parse(userFromCookie)
+      } catch {
+        console.error("Couldn't parse intercom user cookie")
+        boot()
+      }
+
+      boot({
+        name: user.name,
+        email: user.email,
+        avatar: {
+          type: 'avatar',
+          imageUrl: user.imageUrl,
+        },
+        userId: user.id,
+        company: user.company,
+        companies: user.companies,
+      })
+    } else {
+      boot()
+    }
   }, [boot])
 
   React.useEffect(() => {
-    if (typeof window === 'undefined') return
-
     const isChatEnabled = new URLSearchParams(window.location.search).get(
       'chat'
     )
